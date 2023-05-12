1f29d86ff28bb50eb51d35aa8be230b09f80d140
Kaustubh Bhargava <kaustubhbhargava5@gamil.com>
2023-05-12 16:04:38 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,24 @@
+
+from django.db import models
+# Create your models here.
+
+
+class ProfileManager(models.Manager):
+    def create_profile(self,username,mobile_number):
+        profile = self.create(username=username,mobile_number=mobile_number)
+        return profile
+
+class Profile(models.Model):
+    username = models.CharField(max_length=20)   
+    mobile_number = models.CharField(max_length=13, unique=True)
+    user_bio = models.TextField(max_length=50,blank=True)
+    instagram_link = models.URLField(max_length=50,blank=True)
+    facebook_link = models.URLField(max_length=50,blank=True)
+    profile_image = models.ImageField(upload_to='profile_pic/')
+
+    objects = ProfileManager()
+
+    def __str__(self) -> str:
+        return self.username
+    
+    
---
+++ accounts/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from .forms import LoginForm,CreateUserForm
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User,auth
+from .models import Profile
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Invalid credentials','form':LoginForm})
+        
+    else:
+        return render(request,'accounts/login.html',{'form':LoginForm})
+    
+def register_user(request):  
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        number = request.POST['number']
+        f_name = request.POST['firstname']
+        l_name = request.POST['lastname']
+        password = request.POST['password']
+        confirm_password = request.POST['confirmpassword']
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                return render(request,'accounts/register.html',{'error':'Username Already Taken','form':CreateUserForm})
+            user = User.objects.create_user(username=username,email=email,first_name=f_name,last_name=l_name,password=password)
+            profile = Profile.objects.create_profile(username=username,mobile_number=number)
+            user.save()
+            
+            return redirect('/')
+        else:
+            return render(request,'accounts/register.html',{'error':'Password do not match','form':CreateUserForm})
+    else:
+        return render(request,'accounts/register.html',{'form':CreateUserForm})
+        
+def logout_page(request):
+    logout(request)
+    return redirect('/')
